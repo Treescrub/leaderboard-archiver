4cbd5c03bef5adf631d4993de64b33fcb8ed0349
Treescrub <treescrub@gmail.com>
2024-09-22 15:14:55 -0600

    Add hello world application

---
+++ src/main/java/com/treescrub/App.java
@@ -0,0 +1,21 @@
+package com.treescrub;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("javafx.fxml"));
+        Scene scene = new Scene(fxmlLoader. load());
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
