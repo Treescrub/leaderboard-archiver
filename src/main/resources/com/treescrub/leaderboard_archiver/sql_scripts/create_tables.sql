4bab4ff194dba00a39b30adf2d653e39eac07a2a
Treescrub <treescrub@gmail.com>
2025-02-18 01:48:18 -0700

    Update SQL table creation script for MySQL

--- src/main/resources/com/treescrub/leaderboard_archiver/sql_scripts/create_tables.sql
+++ src/main/resources/com/treescrub/leaderboard_archiver/sql_scripts/create_tables.sql
@@ -1,104 +1,115 @@
-CREATE TABLE "archives" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"start_time"	INTEGER NOT NULL,
-	"end_time"	INTEGER,
-	"total_runs"	INTEGER,
-	"total_categories"	INTEGER,
-	"game_id"	TEXT NOT NULL,
-	"game_name"	TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS archives (
+	id	                INTEGER PRIMARY KEY AUTO_INCREMENT UNIQUE,
+	uuid                BINARY(16) NOT NULL,
+
+	start_time	        TIMESTAMP NOT NULL,
+	end_time	        TIMESTAMP NOT NULL,
+	total_runs	        INTEGER NOT NULL,
+	total_categories	INTEGER NOT NULL,
+	game_id	            VARCHAR(16) NOT NULL,
+	game_name	        TEXT NOT NULL
 );
 
-CREATE TABLE "players" (
-	"archive_id"	INTEGER NOT NULL,
-	"run_id"	TEXT NOT NULL,
-	"type"	TEXT NOT NULL,
-	"name"	TEXT NOT NULL,
-	"id"	TEXT NOT NULL,
-	PRIMARY KEY("archive_id","run_id"),
-	FOREIGN KEY("archive_id") REFERENCES "archives"("id"),
-	FOREIGN KEY("run_id") REFERENCES "runs"("id") DEFERRABLE INITIALLY DEFERRED
+CREATE TABLE IF NOT EXISTS players (
+	archive_id  INTEGER NOT NULL,
+	run_id	    VARCHAR(16) NOT NULL,
+	type	    VARCHAR(5) NOT NULL,
+	name	    TEXT NOT NULL,
+	id	        TEXT NOT NULL,
+
+	PRIMARY KEY(archive_id, run_id),
+	FOREIGN KEY(archive_id) REFERENCES archives(id),
+	FOREIGN KEY(run_id, archive_id) REFERENCES runs(id, archive_id)
 );
 
-CREATE TABLE "runs" (
-	"id"	TEXT NOT NULL,
-	"archive_id"	INTEGER NOT NULL,
-	"archive_time"	INTEGER NOT NULL,
-	"category_id"	TEXT NOT NULL,
-	"category_name"	TEXT NOT NULL,
-	"level_id"	INTEGER,
-	"level_name"	INTEGER,
-	"comment"	TEXT,
-	"submission_time"	INTEGER,
-	"date_ran"	INTEGER,
-	PRIMARY KEY("id","archive_id"),
-	FOREIGN KEY("archive_id") REFERENCES "archives"("id")
+CREATE TABLE IF NOT EXISTS runs (
+	id	            VARCHAR(16) NOT NULL,
+	archive_id	    INTEGER NOT NULL,
+	archive_time	INTEGER NOT NULL,
+	category_id	    VARCHAR(16) NOT NULL,
+	category_name	TEXT NOT NULL,
+	level_id	    INTEGER,
+	level_name	    TEXT,
+	comment	        TEXT,
+	submission_time	TIMESTAMP,
+	date_ran	    DATETIME,
+
+	PRIMARY KEY(id, archive_id),
+	FOREIGN KEY(archive_id) REFERENCES archives(id)
 );
 
-CREATE TABLE "splits" (
-	"archive_id"	INTEGER NOT NULL,
-	"run_id"	TEXT NOT NULL,
-	"url"	TEXT NOT NULL,
-	PRIMARY KEY("archive_id","run_id"),
-	FOREIGN KEY("archive_id") REFERENCES "archives"("id"),
-	FOREIGN KEY("run_id") REFERENCES "runs"("id") DEFERRABLE INITIALLY DEFERRED
+CREATE TABLE IF NOT EXISTS splits (
+	archive_id	INTEGER NOT NULL,
+	run_id	    VARCHAR(16) NOT NULL,
+	url	        TEXT NOT NULL,
+
+	PRIMARY KEY(archive_id, run_id),
+	FOREIGN KEY(archive_id) REFERENCES archives(id),
+	FOREIGN KEY(run_id, archive_id) REFERENCES runs(id, archive_id)
 );
 
-CREATE TABLE "status" (
-	"archive_id"	INTEGER NOT NULL,
-	"run_id"	TEXT NOT NULL,
-	"status"	TEXT NOT NULL,
-	"examiner_id"	INTEGER,
-	"examiner_name"	TEXT,
-	"verify_date"	INTEGER,
-	PRIMARY KEY("run_id","archive_id"),
-	FOREIGN KEY("archive_id") REFERENCES "archives"("id"),
-	FOREIGN KEY("run_id") REFERENCES "runs"("id") DEFERRABLE INITIALLY DEFERRED
+CREATE TABLE IF NOT EXISTS status (
+	archive_id	    INTEGER NOT NULL,
+	run_id	        VARCHAR(16) NOT NULL,
+	status	        VARCHAR(8) NOT NULL,
+	examiner_id	    VARCHAR(16),
+	examiner_name	TEXT,
+	verify_date	    TIMESTAMP,
+
+	PRIMARY KEY(run_id, archive_id),
+	FOREIGN KEY(archive_id) REFERENCES archives(id),
+	FOREIGN KEY(run_id, archive_id) REFERENCES runs(id, archive_id)
 );
 
-CREATE TABLE "systems" (
-	"archive_id"	INTEGER NOT NULL,
-	"run_id"	TEXT NOT NULL,
-	"platform_name"	TEXT NOT NULL,
-	"emulated"	INTEGER NOT NULL,
-	"region_name"	TEXT,
-	PRIMARY KEY("archive_id","run_id"),
-	FOREIGN KEY("archive_id") REFERENCES "archives"("id"),
-	FOREIGN KEY("run_id") REFERENCES "runs"("id") DEFERRABLE INITIALLY DEFERRED
+CREATE TABLE IF NOT EXISTS systems (
+	archive_id	    INTEGER NOT NULL,
+	run_id	        VARCHAR(16) NOT NULL,
+	platform_name	TEXT NOT NULL,
+	emulated	    BOOLEAN NOT NULL,
+	region_name	    TEXT,
+
+	PRIMARY KEY(archive_id, run_id),
+	FOREIGN KEY(archive_id) REFERENCES archives(id),
+	FOREIGN KEY(run_id, archive_id) REFERENCES runs(id, archive_id)
 );
 
-CREATE TABLE "times" (
-	"archive_id"	INTEGER NOT NULL,
-	"run_id"	TEXT NOT NULL,
-	"primary_time"	REAL NOT NULL,
-	"real_time"	REAL,
-	"real_noloads_time"	REAL,
-	"ingame_time"	REAL,
-	PRIMARY KEY("run_id","archive_id"),
-	FOREIGN KEY("archive_id") REFERENCES "archives"("id"),
-	FOREIGN KEY("run_id") REFERENCES "runs"("id") DEFERRABLE INITIALLY DEFERRED
+CREATE TABLE IF NOT EXISTS times (
+	archive_id	        INTEGER NOT NULL,
+	run_id	            VARCHAR(16) NOT NULL,
+	primary_time	    REAL NOT NULL,
+	real_time	        REAL,
+	real_noloads_time	REAL,
+	ingame_time	        REAL,
+
+	PRIMARY KEY(run_id, archive_id),
+	FOREIGN KEY(archive_id) REFERENCES archives(id),
+	FOREIGN KEY(run_id, archive_id) REFERENCES runs(id, archive_id)
 );
 
-CREATE TABLE "values" (
-	"archive_id"	INTEGER NOT NULL,
-	"run_id"	TEXT NOT NULL,
-	"name"	TEXT NOT NULL,
-	"value"	TEXT NOT NULL,
-	PRIMARY KEY("archive_id","run_id"),
-	FOREIGN KEY("archive_id") REFERENCES "archives"("id"),
-	FOREIGN KEY("run_id") REFERENCES "runs"("id") DEFERRABLE INITIALLY DEFERRED
+CREATE TABLE IF NOT EXISTS variables (
+	archive_id  INTEGER NOT NULL,
+	run_id	    VARCHAR(16) NOT NULL,
+	name	    TEXT NOT NULL,
+	value	    TEXT NOT NULL,
+
+	PRIMARY KEY(archive_id, run_id),
+	FOREIGN KEY(archive_id) REFERENCES archives(id),
+	FOREIGN KEY(run_id, archive_id) REFERENCES runs(id, archive_id)
 );
 
-CREATE TABLE "versions" (
-	"version"	TEXT NOT NULL UNIQUE,
-	"upgrade_time"	INTEGER,
-	PRIMARY KEY("version")
+CREATE TABLE IF NOT EXISTS versions (
+	version	        VARCHAR(16) NOT NULL UNIQUE,
+	upgrade_time	TIMESTAMP,
+
+	PRIMARY KEY(version)
 );
 
-CREATE TABLE "videos" (
-	"archive_id"	INTEGER NOT NULL,
-	"run_id"	TEXT NOT NULL,
-	"url"	TEXT NOT NULL,
-	PRIMARY KEY("archive_id","run_id"),
-	FOREIGN KEY("archive_id") REFERENCES "archives"("id"),
-	FOREIGN KEY("run_id") REFERENCES "runs"("id") DEFERRABLE INITIALLY DEFERRED
+CREATE TABLE IF NOT EXISTS videos (
+	archive_id	INTEGER NOT NULL,
+	run_id	    VARCHAR(16) NOT NULL,
+	url	        TEXT NOT NULL,
+
+	PRIMARY KEY(archive_id, run_id),
+	FOREIGN KEY(archive_id) REFERENCES archives(id),
+	FOREIGN KEY(run_id, archive_id) REFERENCES runs(id, archive_id)
 );
