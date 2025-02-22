9a6666e7e48629259d400e8e625c6205e71c029d
Treescrub <treescrub@gmail.com>
2025-02-22 11:34:53 -0700

    Even more table script revisions

--- src/main/resources/com/treescrub/leaderboard_archiver/sql_scripts/create_tables.sql
+++ src/main/resources/com/treescrub/leaderboard_archiver/sql_scripts/create_tables.sql
@@ -1,28 +1,17 @@
-CREATE TABLE IF NOT EXISTS archives (
+CREATE TABLE IF NOT EXISTS archive (
 	id	                INTEGER PRIMARY KEY AUTO_INCREMENT UNIQUE,
-	uuid                BINARY(16) NOT NULL,
+	uuid                CHAR(36) NOT NULL,
 
 	start_time	        BIGINT NOT NULL,
 	end_time	        BIGINT NOT NULL,
 	total_runs	        INTEGER NOT NULL,
 	total_categories	INTEGER NOT NULL,
 	game_id	            VARCHAR(16) NOT NULL,
-	game_name	        TEXT NOT NULL
+	game_name	        TEXT NOT NULL,
+	INDEX(uuid)
 );
 
-CREATE TABLE IF NOT EXISTS players (
-	archive_id  INTEGER NOT NULL,
-	run_id	    VARCHAR(16) NOT NULL,
-	type	    VARCHAR(5) NOT NULL,
-	name	    TEXT NOT NULL,
-	id	        VARCHAR(16) NOT NULL,
-
-	PRIMARY KEY(archive_id, run_id),
-	FOREIGN KEY(archive_id) REFERENCES archives(id),
-	FOREIGN KEY(run_id, archive_id) REFERENCES runs(id, archive_id)
-);
-
-CREATE TABLE IF NOT EXISTS runs (
+CREATE TABLE IF NOT EXISTS run (
 	id	            VARCHAR(16) NOT NULL,
 	archive_id	    INTEGER NOT NULL,
 	archive_time	BIGINT NOT NULL,
@@ -35,17 +24,29 @@ CREATE TABLE IF NOT EXISTS runs (
 	date_ran	    DATE,
 
 	PRIMARY KEY(id, archive_id),
-	FOREIGN KEY(archive_id) REFERENCES archives(id)
+	FOREIGN KEY(archive_id) REFERENCES archive(id)
 );
 
-CREATE TABLE IF NOT EXISTS splits (
+CREATE TABLE IF NOT EXISTS player (
+	archive_id  INTEGER NOT NULL,
+	run_id	    VARCHAR(16) NOT NULL,
+	type	    VARCHAR(5) NOT NULL,
+	name	    TEXT NOT NULL,
+	id	        VARCHAR(16) NOT NULL,
+
+	PRIMARY KEY(archive_id, run_id),
+	FOREIGN KEY(archive_id) REFERENCES archive(id),
+	FOREIGN KEY(run_id, archive_id) REFERENCES run(id, archive_id)
+);
+
+CREATE TABLE IF NOT EXISTS split (
 	archive_id	INTEGER NOT NULL,
 	run_id	    VARCHAR(16) NOT NULL,
 	url	        TEXT NOT NULL,
 
 	PRIMARY KEY(archive_id, run_id),
-	FOREIGN KEY(archive_id) REFERENCES archives(id),
-	FOREIGN KEY(run_id, archive_id) REFERENCES runs(id, archive_id)
+	FOREIGN KEY(archive_id) REFERENCES archive(id),
+	FOREIGN KEY(run_id, archive_id) REFERENCES run(id, archive_id)
 );
 
 CREATE TABLE IF NOT EXISTS status (
@@ -57,11 +58,11 @@ CREATE TABLE IF NOT EXISTS status (
 	verify_date	    BIGINT,
 
 	PRIMARY KEY(run_id, archive_id),
-	FOREIGN KEY(archive_id) REFERENCES archives(id),
-	FOREIGN KEY(run_id, archive_id) REFERENCES runs(id, archive_id)
+	FOREIGN KEY(archive_id) REFERENCES archive(id),
+	FOREIGN KEY(run_id, archive_id) REFERENCES run(id, archive_id)
 );
 
-CREATE TABLE IF NOT EXISTS systems (
+CREATE TABLE IF NOT EXISTS run_system (
 	archive_id	    INTEGER NOT NULL,
 	run_id	        VARCHAR(16) NOT NULL,
 	platform_name	TEXT NOT NULL,
@@ -69,11 +70,11 @@ CREATE TABLE IF NOT EXISTS systems (
 	region_name	    TEXT,
 
 	PRIMARY KEY(archive_id, run_id),
-	FOREIGN KEY(archive_id) REFERENCES archives(id),
-	FOREIGN KEY(run_id, archive_id) REFERENCES runs(id, archive_id)
+	FOREIGN KEY(archive_id) REFERENCES archive(id),
+	FOREIGN KEY(run_id, archive_id) REFERENCES run(id, archive_id)
 );
 
-CREATE TABLE IF NOT EXISTS times (
+CREATE TABLE IF NOT EXISTS run_time (
 	archive_id	        INTEGER NOT NULL,
 	run_id	            VARCHAR(16) NOT NULL,
 	primary_time	    REAL NOT NULL,
@@ -82,34 +83,27 @@ CREATE TABLE IF NOT EXISTS times (
 	ingame_time	        REAL,
 
 	PRIMARY KEY(run_id, archive_id),
-	FOREIGN KEY(archive_id) REFERENCES archives(id),
-	FOREIGN KEY(run_id, archive_id) REFERENCES runs(id, archive_id)
+	FOREIGN KEY(archive_id) REFERENCES archive(id),
+	FOREIGN KEY(run_id, archive_id) REFERENCES run(id, archive_id)
 );
 
-CREATE TABLE IF NOT EXISTS variables (
+CREATE TABLE IF NOT EXISTS variable (
 	archive_id  INTEGER NOT NULL,
 	run_id	    VARCHAR(16) NOT NULL,
 	name	    TEXT NOT NULL,
 	value	    TEXT NOT NULL,
 
 	PRIMARY KEY(archive_id, run_id),
-	FOREIGN KEY(archive_id) REFERENCES archives(id),
-	FOREIGN KEY(run_id, archive_id) REFERENCES runs(id, archive_id)
+	FOREIGN KEY(archive_id) REFERENCES archive(id),
+	FOREIGN KEY(run_id, archive_id) REFERENCES run(id, archive_id)
 );
 
-CREATE TABLE IF NOT EXISTS versions (
-	version	        TEXT NOT NULL UNIQUE,
-	upgrade_time	TIMESTAMP,
-
-	PRIMARY KEY(version)
-);
-
-CREATE TABLE IF NOT EXISTS videos (
+CREATE TABLE IF NOT EXISTS video (
 	archive_id	INTEGER NOT NULL,
 	run_id	    VARCHAR(16) NOT NULL,
 	url	        TEXT NOT NULL,
 
 	PRIMARY KEY(archive_id, run_id),
-	FOREIGN KEY(archive_id) REFERENCES archives(id),
-	FOREIGN KEY(run_id, archive_id) REFERENCES runs(id, archive_id)
+	FOREIGN KEY(archive_id) REFERENCES archive(id),
+	FOREIGN KEY(run_id, archive_id) REFERENCES run(id, archive_id)
 );
